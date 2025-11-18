4e5888fb6364c5a15ad797f8605591bc592c3a0e
namiyu5 <iahmed0610@gmail.com>
2025-11-18 14:09:17 +0000

    Enhance admin interface for Post and Comment models; add Summernote integration and update model options

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
-
-
 
 # Create your models here.
 class Post(models.Model):
@@ -33,9 +31,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    new_field = models.SlugField()
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
+        
