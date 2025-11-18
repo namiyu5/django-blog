24fc24bf1b09552fec5c1001ef16e8c518652ca2
namiyu5 <iahmed0610@gmail.com>
2025-11-18 13:29:10 +0000

    Reorder admin URL path in codestar project; standardize quotes in blog URLs

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
