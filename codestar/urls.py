6cf80cf54e19bfc83185ff109f5a9d63d743d94b
namiyu5 <iahmed0610@gmail.com>
2025-11-18 12:45:11 +0000

    Add Comment model and update admin registration; create post list view and template

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views as blog_views
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog_views.blog_home, name='blog'),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
 
 if settings.DEBUG:
